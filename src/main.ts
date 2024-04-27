f2523bf3a9cf8ea14f10265be6e689ad0ba729dd
Abdelhady <hadymohamed2002@gmail.com>
2024-04-27 17:40:05 +0300

    add: fastify configs & morgan logger

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as morgan from 'morgan';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule,new FastifyAdapter());
 
   //swagger configration
   const swaggerConfig = new DocumentBuilder()
     .setTitle('ITunes Integration')
     .setDescription('The ITunes integration API')
     .setVersion('1.0')
-    // .addTag('media')
     .build();
   
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api/swagger', app, swaggerDocument);
+
+  //add morgan
+  app.use(morgan('dev'));
   
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
