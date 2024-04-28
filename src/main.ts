2ab032b343643ef539599ae606b0143c3783a318
Abdelhady <hadymohamed2002@gmail.com>
2024-04-28 08:12:36 +0300

    add: address 0.0.0.0 trying fix 'No open ports detected on 0.0.0.0'

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,9 @@ async function bootstrap() {
 
   app.enableCors();
   
-  await app.listen(process.env.PORT || 3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0' ,async ()=>{
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  });
+  
 }
 bootstrap();
